2fa0a977b97a366caa2672aecfdfd41f79c5b3d0
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2016-10-27 11:05:22 +0100

    Create addrspace casts when casting to and from capabilities

--- lib/Sema/SemaExpr.cpp
+++ lib/Sema/SemaExpr.cpp
@@ -5762,7 +5762,10 @@ CastKind Sema::PrepareScalarCast(ExprResult &Src, QualType DestTy) {
       unsigned DestAS = DestTy->getPointeeType().getAddressSpace();
       if (SrcAS != DestAS)
         return CK_AddressSpaceConversion;
-      return CK_BitCast;
+      else if (SrcTy->isMemoryCapabilityType(Context) != DestTy->isMemoryCapabilityType(Context))
+        return CK_AddressSpaceConversion;
+      else
+        return CK_BitCast;
     }
     case Type::STK_BlockPointer:
       return (SrcKind == Type::STK_BlockPointer
