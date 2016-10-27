fd8f5c3838e73f5b9d00b136781d980ae8c07fed
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2016-10-27 12:24:31 +0100

    return Incompatible when trying to implicitly cast to or from a capability data pointer

--- lib/Sema/SemaExpr.cpp
+++ lib/Sema/SemaExpr.cpp
@@ -7030,7 +7030,7 @@ Sema::CheckAssignmentConstraints(QualType LHSType, ExprResult &RHS,
         if (LHSPointer->isFunctionPointerType() && RHSPointer->isFunctionPointerType())
           Kind = CK_AddressSpaceConversion;
         else
-          Kind = CK_Invalid;
+          return Incompatible;
       } else
         Kind = CK_BitCast;
       return checkPointerTypesForAssignment(*this, LHSType, RHSType);
