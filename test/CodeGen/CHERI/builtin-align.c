0122263ed95099b8eac335f91156d4a84f93e91f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-12-21 20:02:34 +0100

    Properly optimize __uintcap_t case for __builtin_align
    The fixes in LLVM are not enough, we also need to run the
    CHERICapFoldIntrinsicsPass one more time after the inline functions have
    been simplified.

--- test/CodeGen/CHERI/builtin-align.c
+++ test/CodeGen/CHERI/builtin-align.c
@@ -5,8 +5,8 @@
 
 // RUN: %cheri_cc1 -DTEST_CAP -Wno-tautological-compare -o - -O0 -emit-llvm %s     | FileCheck %s -check-prefixes CHECK,CAP,SLOW -enable-var-scope
 // RUN: %cheri_cc1 -DTEST_CAP -Wno-tautological-compare -o - -O2 -emit-llvm %s     | FileCheck %s -check-prefixes CHECK,CAP,OPT,CAP-OPT  -enable-var-scope
-// RUNNOT: %cheri_cc1 -DTEST_UINTCAP -Wno-tautological-compare -o - -O0 -emit-llvm %s | FileCheck %s -check-prefixes CHECK,CAP,SLOW -enable-var-scope
-// RUNNOT: %cheri_cc1 -DTEST_UINTCAP -Wno-tautological-compare -o - -O2 -emit-llvm %s | FileCheck %s -check-prefixes CHECK,CAP,OPT,UINTCAP-OPT  -enable-var-scope
+// RUN: %cheri_cc1 -DTEST_UINTCAP -Wno-tautological-compare -o - -O0 -emit-llvm %s | FileCheck %s -check-prefixes CHECK,CAP,SLOW -enable-var-scope
+// RUN: %cheri_cc1 -DTEST_UINTCAP -Wno-tautological-compare -o - -O2 -emit-llvm %s | FileCheck %s -check-prefixes CHECK,CAP,OPT,UINTCAP-OPT  -enable-var-scope
 
 void *add(void *arg, __PTRDIFF_TYPE__ diff) {
   return (char *)arg + diff;
@@ -178,7 +178,7 @@ TYPE inline_align_down(void) {
   // LONG-OPT: ret i64 96
   // PTR-OPT: ret i8* inttoptr (i64 96 to i8*)
   // CAP-OPT: ret i8 addrspace(200)* inttoptr (i64 96 to i8 addrspace(200)*)
-  // UINTCAP-OPT: ret i8 addrspace(200)* inttoptr (i64 96 to i8 addrspace(200)*)
+  // UINTCAP-OPT: tail call i8 addrspace(200)* @llvm.cheri.cap.offset.set(i8 addrspace(200)* null, i64 96)
   return align_down((TYPE)100, 32);
 }
 
@@ -188,6 +188,8 @@ TYPE inline_p2align_down(void) {
   // LONG-OPT: ret i64 0
   // PTR-OPT: ret i8* null
   // CAP-OPT: ret i8 addrspace(200)* null
+  // FIXME: this should be optimized to return null:
+  // UINTCAP-OPT: tail call i8 addrspace(200)* @llvm.cheri.cap.offset.set(i8 addrspace(200)* null, i64 0)
   return p2align_down((TYPE)100, 10);
 }
 
@@ -197,6 +199,7 @@ TYPE inline_align_up(void) {
   // LONG-OPT: ret i64 128
   // PTR-OPT: ret i8* inttoptr (i64 128 to i8*)
   // CAP-OPT: ret i8 addrspace(200)* inttoptr (i64 128 to i8 addrspace(200)*)
+  // UINTCAP-OPT: tail call i8 addrspace(200)* @llvm.cheri.cap.offset.set(i8 addrspace(200)* null, i64 128)
   return align_up((TYPE)100, 32);
 }
 
@@ -206,6 +209,7 @@ TYPE inline_p2align_up(void) {
   // LONG-OPT: ret i64 1024
   // PTR-OPT: ret i8* inttoptr (i64 1024 to i8*)
   // CAP-OPT: ret i8 addrspace(200)* inttoptr (i64 1024 to i8 addrspace(200)*)
+  // UINTCAP-OPT: tail call i8 addrspace(200)* @llvm.cheri.cap.offset.set(i8 addrspace(200)* null, i64 1024)
   return p2align_up((TYPE)100, 10);
 }
 
